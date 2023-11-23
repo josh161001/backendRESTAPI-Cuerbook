85a8466d2cfdca549759d5137eb7f5b7905c71a9
josh161001 <estrella161610@gmail.com>
2023-11-23 16:01:08 -0600

    add client}

--- src/app.module.ts
+++ src/app.module.ts
@@ -39,7 +39,7 @@ import { join } from 'path';
       envFilePath: '.env',
     }),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'dist'),
+      rootPath: join(__dirname, '..', 'client'),
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
