b7badccd8b26e071b8798559ecdc4855f452a6b8
josh161001 <estrella161610@gmail.com>
2023-11-23 14:50:33 -0600

    add root

--- src/main.ts
+++ src/main.ts
@@ -25,14 +25,6 @@ async function bootstrap() {
     },
   });
 
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     transform: true,
-  //     whitelist: true,
-  //     forbidNonWhitelisted: true,
-  //   }),
-  // );
-
   app.use('/upload', express.static(join(__dirname, '../../', 'upload')));
 
   await app.listen(AppModule.port);
